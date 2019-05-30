17d81326bda84d08b8c79ce11c1024669d83cf7f
Todd Wolfson <todd@twolfson.com>
2019-05-30 15:01:55 -0700

    Documented caching issues for future self

--- server/index.js
+++ server/index.js
@@ -2,16 +2,19 @@
 // DEV: We could increase boot time by pre-compiling all views to functions
 const pug = require('pug');
 
+// WARNING: `now dev` seems to not cache bust when we adjust `index.pug`,
+//   even with this `renderFile` inline approach...
+
 // Preload our views
-// TODO: Set up NODE_ENV for project
+// TODO: NODE_ENV is set up by default by `now` but we should make it explicit in a file too
 let pugOptions = {
-  pretty: process.env.NODE_ENV !== 'production'
+  cache: process.env.NODE_ENV === 'production',
+  pretty: process.env.NODE_ENV !== 'production',
 };
-const indexView = pug.compileFile(__dirname + '/views/index.pug', pugOptions);
 
 // Define our main handler
 function main(req, res) {
-  // TODO: Add caching to response
-  res.end(indexView());
+  // TODO: Add cache header to response
+  res.end(pug.renderFile(__dirname + '/views/index.pug', pugOptions));
 }
 module.exports = main;
