da6fbcd07578ab6a8b3a05d362d685eb88ea3995
Todd Wolfson <todd@twolfson.com>
2019-05-30 12:47:29 -0700

    Set up build with pug

--- server/index.js
+++ server/index.js
@@ -1,5 +1,16 @@
+// Load in our dependencies
+// DEV: We could increase boot time by pre-compiling all views to functions
+const pug = require('pug');
+
+// Preload our views
+// TODO: Set up NODE_ENV for project
+let pugOptions = {
+  pretty: process.env.NODE_ENV === 'production'
+};
+const indexView = pug.compileFile(__dirname + '/views/index.pug', pugOptions);
+
 // Define our main handler
 function main(req, res) {
-  res.end('Hello world!');
+  res.end(indexView());
 }
 module.exports = main;
