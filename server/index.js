d2bbd01edfabffa3baa65597fc13d71e41e16716
Todd Wolfson <todd@twolfson.com>
2019-06-01 01:09:04 -0700

    Moved to express for app

--- server/index.js
+++ server/index.js
@@ -1,26 +1,24 @@
 // Load in our dependencies
 // DEV: We could increase boot time by pre-compiling all views to functions
 const Glassdoor = require('./models/glassdoor');
-const finalhandler = require('finalhandler');
-const pug = require('pug');
-const Router = require('router');
+const express = require('express');
 const semverVersion = require('../package.json').version;
 
 // Define common constants
 // DEV: NODE_ENV is set up by `now` (e.g. `development`, `production`)
 const PRODUCTION_TTL = 10 * 60; // 10 minutes
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-let pugOptions = {
-  cache: IS_PRODUCTION,
-  pretty: !IS_PRODUCTION,
-};
-let commonLocals = {
+
+// Build out our app
+var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+app.locals = {
   defaultQuery: 'Google',
   semverVersion
 };
 
-// Build out our router
-let router = Router();
+// Define our routes
 function setCommonCache(req) {
   // Set up caching for our response
   // https://zeit.co/docs/v2/deployments/concepts/cdn-and-global-distribution/#full-cdn
@@ -28,18 +26,13 @@ function setCommonCache(req) {
     res.setHeader('Cache-Control', `public, s-maxage=${PRODUCTION_TTL}, max-age=${PRODUCTION_TTL}`);
   }
 }
-function render(res, filepath, locals) {
-  // DEV: We separate `compileFile` from rendering to avoid conflating options into `locals` accidentally
-  let viewFn = pug.compileFile(filepath, pugOptions);
-  return viewFn(Object.assign({}, commonLocals, locals));
-}
-router.get('/', async function rootShow (req, res) {
+app.get('/', async function rootShow (req, res) {
   setCommonCache(req);
   let glassdoorInfo = await Glassdoor.findFirstCompany(req, commonLocals.defaultQuery);
-  res.end(render(__dirname + '/views/index.pug', { query, glassdoorInfo }));
+  res.render('index', { query, glassdoorInfo }));
+  // let glassdoorInfo = await Glassdoor.findFirstCompany(req, query);
+  // res.render('index', { query, glassdoorInfo }));
 });
 
-// Define our main handler
-async function main(req, res) {
-}
-module.exports = main;
+// Export our app as our module.exports
+module.exports = app;
