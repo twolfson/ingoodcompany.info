9b5c2a1b4e1cda98fbcc05424bd5328902d2d637
Todd Wolfson <todd@twolfson.com>
2019-06-07 01:20:42 -0700

    Attempting to see if we get an error reported with Raven

--- tmp.js
+++ tmp.js
@@ -1,17 +1,18 @@
 const assert = require('assert');
 const express = require('express');
 const app = express();
-const Sentry = require('@sentry/node');
+// const Sentry = require('@sentry/node');
+const Raven = require('raven');
 
 const SENTRY_SERVER_DSN = process.env.SENTRY_SERVER_DSN;
 assert(SENTRY_SERVER_DSN);
-Sentry.init({ dsn: SENTRY_SERVER_DSN });
+// Sentry.init({ dsn: SENTRY_SERVER_DSN });
+Raven.config(SENTRY_SERVER_DSN).install();
 
 // The request handler must be the first middleware on the app
-app.use(Sentry.Handlers.requestHandler());
+// app.use(Sentry.Handlers.requestHandler());
+app.use(Raven.requestHandler());
 
-// The error handler must be before any other error middleware
-app.use(Sentry.Handlers.errorHandler());
 
 // Optional fallthrough error handler
 app.use(function onError(err, req, res, next) {
@@ -22,10 +23,14 @@ app.use(function onError(err, req, res, next) {
 });
 
 app.get('/debug-sentry', function mainHandler(req, res) {
-  process.nextTick(() => {
+  // process.nextTick(() => {
     throw new Error('My first Sentry error!');
-  });
+  // });
 });
+
+// The error handler must be before any other error middleware
+// app.use(Sentry.Handlers.errorHandler());
+app.use(Raven.errorHandler());
 
 app.listen(3000);
 
